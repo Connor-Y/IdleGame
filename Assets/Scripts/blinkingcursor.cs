a4c5678f485b96242d7f2f06a79c9248f469ae4c
MichaelWoloszyn <michael@woloszyn.ca>
2016-08-15 14:04:24 -0400

    Added an animation for the computer screen

---
+++ Assets/Scripts/blinkingcursor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class blinkingcursor : MonoBehaviour {
+    TextMesh txt;
+    float timer = 0.5f;
+	// Use this for initialization
+	void Start () {
+        txt = gameObject.GetComponent<TextMesh>();
+        txt.text = ">";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer < 0f)
+        {
+            if (txt.text == ">")
+            {
+                txt.text = ">_";
+            }
+            else
+            {
+                txt.text = ">";
+            }
+            timer = 0.5f;
+        }
+	}
+}
