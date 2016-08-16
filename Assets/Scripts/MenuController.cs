e5bfd5ae507655c40d5602d46eaba47bf5aaeada
Connor-Y <connor.y@gmail.com>
2016-08-15 21:37:25 -0400

    Basic popup menu added, right pane now for notifications (not implemented

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+
+    private bool isShowing;
+    public GameObject upgradeMenuPanel;
+
+    // Use this for initialization
+    void Start () {
+        isShowing = false;
+
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void activateMenu()
+    {
+        if (isShowing)
+        {
+            isShowing = false;
+            upgradeMenuPanel.SetActive(false);
+            return;
+        }
+
+        isShowing = true;
+        upgradeMenuPanel.SetActive(true);
+
+
+
+    }
+}
